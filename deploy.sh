dacca9889f435069bf64c9998d5cb87b46611813
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-03-19 13:51:25 -0400

    try for pages

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:patricksbrown1992/Currency-Exchange-Rater.git master:gh-pages
+
+cd -
